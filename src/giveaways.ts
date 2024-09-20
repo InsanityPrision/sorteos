8619c30437dda75bbfb3d0928e73035f59b58cba
Erik Riquelme <erikone2311@gmail.com>
2024-09-20 21:37:53 +0200

    Add login-user

--- src/giveaways.ts
+++ src/giveaways.ts
@@ -1,5 +1,29 @@
+import { userInfo } from "node:os";
 import { askUser } from "./askUser.js";
 import { programData, saveData } from "./storage.js";
 
 import { Giveaway } from "./types.js";
 import { askUserNewGiveawayData } from "./ui.js";
+
+export const loginUser = (email: string, password: string): void => {
+  const findAdmin = programData.users.find((userAdmin) => userAdmin.isAdmin);
+  const findName = programData.users.find((userName) => userName.name);
+  const findEmail = programData.users.find((userEmail) => userEmail.email);
+  const isSameEmail = findEmail?.email === email;
+  const findPassword = programData.users.find(
+    (userPassword) => userPassword.password
+  );
+  const isSamePassword = findPassword?.password === password;
+
+  if (isSameEmail === false || isSamePassword === false) {
+    console.log("Usuario no registrado o credenciales incorrectas");
+    process.exitCode = 1;
+  }
+
+  programData.userEmail = findEmail!.email;
+  programData.isAdmin = findAdmin!.isAdmin;
+
+  saveData();
+
+  console.log(`Bienvenido ${findName!.name}`);
+};
